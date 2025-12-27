9f0bbc996a11ed763a759e99daaad869a033b509
hwayungyi90 <hwayoung0716@gmail.com>
2025-12-27 16:45:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 import plotly.graph_objects as go
 import requests
 import time
@@ -490,8 +489,7 @@ class Signature:
     def generate(timestamp, method, uri, secret_key):
         message = "{}.{}.{}".format(timestamp, method, uri)
         hash = hmac.new(bytes(secret_key, "utf-8"), bytes(message, "utf-8"), hashlib.sha256)
-        hash.hexdigest()
-        return base64.b64encode(hash.digest())
+        return base64.b64encode(hash.digest()).decode('utf-8')
 
 
 def get_header(method, uri, api_key, secret_key, customer_id):
